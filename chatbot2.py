1ac6b4536326fa129e0b4444abd278a902deb05c
o000SAI000o <vishwajitdeshmane2004@gmail.com>
2025-04-30 02:19:37 +0530

    Add files via upload

---
+++ chatbot2.py
@@ -0,0 +1,49 @@
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer, ChatterBotCorpusTrainer
+
+# Create chatbot
+my_bot = ChatBot(name='PyBot', read_only=True,
+                 logic_adapters=[
+                     'chatterbot.logic.MathematicalEvaluation',
+                     'chatterbot.logic.BestMatch'])
+
+# Training data
+small_talk = ['hi there!',
+              'hi!',
+              'how do you do?',
+              'how are you?',
+              'i\'m cool.',
+              'fine, you?',
+              'always cool.',
+              'i\'m ok',
+              'glad to hear that.',
+              'i\'m fine',
+              'glad to hear that.',
+              'i feel awesome',
+              'excellent, glad to hear that.',
+              'not so good',
+              'sorry to hear that.',
+              'what\'s your name?',
+              'i\'m pybot. ask me a math question, please.']
+
+math_talk_1 = ['pythagorean theorem',
+               'a squared plus b squared equals c squared.']
+
+math_talk_2 = ['law of cosines',
+               'c**2 = a**2 + b**2 - 2 * a * b * cos(gamma)']
+
+# Train with custom data
+list_trainer = ListTrainer(my_bot)
+for item in (small_talk, math_talk_1, math_talk_2):
+    list_trainer.train(item)
+
+# Optional: train with English corpus
+corpus_trainer = ChatterBotCorpusTrainer(my_bot)
+corpus_trainer.train('chatterbot.corpus.english')
+
+# Test responses
+print(my_bot.get_response("how are you?"))
+print(my_bot.get_response("hi"))
+print(my_bot.get_response("what's your name?"))
+print(my_bot.get_response("ask me something"))
+print(my_bot.get_response("tell me about story"))
